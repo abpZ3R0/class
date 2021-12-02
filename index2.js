c6419e70851e49252462ce913b5f3fab4cb1d187
abpZ3R0 <arnab.fullmatal@gmail.com>
2021-12-02 13:15:18 +0600

    updates

---
+++ index2.js
@@ -0,0 +1,82 @@
+// const arr = [1,2,4,5,6,7,8];
+// const newArr = arr.slice(0);
+// newArr.pop();
+
+// function abc() {
+//     return 3;
+// }
+
+// const newArr = arr.splice(2, 0, abc);
+
+// console.log(arr);
+
+
+// const a = 5;
+// let b = 3;
+
+// function abc(){
+//     var x
+//     const c = 100;
+//     //
+//     //
+//     //
+//     var x = 50;
+//     return function(){
+//         console.log(a)
+//     }
+// }
+
+// abc()();
+// console.log(i)
+
+// for (let i = 0; i < 5; i++) {
+//     console.log(i);
+// }
+
+// console.log(i);
+
+const person = {
+    name: "pritom",
+    age: 28,
+    title: "chy",
+};
+
+// for(x in person){
+//     console.log(x, person[x])
+// }
+
+const keys = Object.keys(person); // [name, age, title]
+
+// const newKeys = keys.map(key => {
+//     console.log(key, person[key])
+// })
+
+
+// const newKeys = keys.map(key => {
+//    return {[key] : person[key]} // {name: "pritom" }
+// })
+
+// console.log(newKeys)
+
+
+
+// const xyz = [
+//     {
+//         id: 1,
+//         name: 'fbsdi sdbf',
+//         age: 40,
+//         profession: '',
+//         favFood: [
+//             {
+//                 id: 1,
+//                 name: "rice"
+//             }
+//         ]
+//     }
+// ]
+
+
+(function add(a){
+    console.log(a)
+})(2)
+// invoke
